09f0774173d18e803360ebd598b3e95db1fdad01
seba-mint <sesanhueza2019@udec.cl>
2025-07-14 23:35:49 -0400

    a

--- src/selection_cuda_wrapper.hpp
+++ src/selection_cuda_wrapper.hpp
@@ -1,12 +1,26 @@
 #pragma once
 #include <cstdint>
+#include <cuda_runtime.h>
 
 void launch_kernel_smh(
-    const uint64_t* d_sketches, const double* d_cards,
-    int N, int m, int n_rows, int n_bands, double tau, int* d_out,
-    int block, int grid);
+    const uint64_t* d_sketches,
+    const double* d_cards,
+    const uint2* d_pairs,
+    int m,
+    int n_rows, int n_bands,
+    int total_pairs,
+    int* d_out,
+    int blockSize,
+    cudaStream_t stream);
 
 void launch_kernel_CBsmh(
-    const uint64_t* d_sketches, const double* d_cards,
-    int N, int m, int n_rows, int n_bands, double tau, int* d_out,
-    int blockSize, int gridSize);
+    const uint64_t* d_sketches,
+    const double* d_cards,
+    const uint2* d_pairs,
+    int m,
+    int n_rows, int n_bands,
+    double tau,
+    int total_pairs,
+    int* d_out,
+    int blockSize,
+    cudaStream_t stream);
